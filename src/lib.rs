bdf1d8ad36742fbada4cd6918468482fbd1c123c
Tibor Prokai <tibor@kotem.com>
2019-09-13 22:32:15 +0200

    CAN implementation added

--- src/lib.rs
+++ src/lib.rs
@@ -35,37 +35,37 @@
 //! ```rust
 //! #![no_std]
 //! #![no_main]
-//! 
+//!
 //! extern crate panic_halt;
-//! 
+//!
 //! use nb::block;
-//! 
+//!
 //! use stm32f1xx_hal::{
 //!     prelude::*,
 //!     pac,
 //!     timer::Timer,
 //! };
 //! use cortex_m_rt::entry;
-//! 
+//!
 //! #[entry]
 //! fn main() -> ! {
 //!     // Get access to the core peripherals from the cortex-m crate
 //!     let cp = cortex_m::Peripherals::take().unwrap();
 //!     // Get access to the device specific peripherals from the peripheral access crate
 //!     let dp = pac::Peripherals::take().unwrap();
-//! 
+//!
 //!     // Take ownership over the raw flash and rcc devices and convert them
 //!     // into the corresponding HAL structs
 //!     let mut flash = dp.FLASH.constrain();
 //!     let mut rcc = dp.RCC.constrain();
-//! 
+//!
 //!     // Freeze the configuration of all the clocks in the system and store
 //!     // the frozen frequencies in `clocks`
 //!     let clocks = rcc.cfgr.freeze(&mut flash.acr);
-//! 
+//!
 //!     // Acquire the GPIOC peripheral
 //!     let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
-//! 
+//!
 //!     // Configure gpio C pin 13 as a push-pull output. The `crh` register is
 //!     // passed to the function in order to configure the port. For pins 0-7,
 //!     // crl should be passed instead.
@@ -73,7 +73,7 @@
 //!     // Configure the syst timer to trigger an update every second
 //!     let mut timer = Timer::syst(cp.SYST, clocks)
 //!         .start_count_down(1.hz());
-//! 
+//!
 //!     // Wait for the timer to trigger an update and change the state of the LED
 //!     loop {
 //!         block!(timer.wait()).unwrap();
@@ -120,6 +120,8 @@ pub mod afio;
 pub mod backup_domain;
 #[cfg(feature = "device-selected")]
 pub mod bb;
+#[cfg(feature = "device-selected")]
+pub mod can;
 #[cfg(feature = "device-selected")]
 pub mod delay;
 #[cfg(feature = "device-selected")]
